553a756a02d0bdab2efbe9c4952c7518cf473e67
pjonathan <pj.natividad@dot.ph>
2023-04-27 10:51:12 +0800

    shortname static page v1

---
+++ js/masonry.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $('.grid').masonry({
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    gutter: 12,
+    horizontalOrder: true,
+    percentPosition: true
+  });
+});
