1f69c35205f2fb768d160a9d701fe9ee507fd230
pjonathan <pj.natividad@dot.ph>
2023-04-28 14:01:51 +0800

    fixed for js v3

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
-  // set the clickable button of shortname
-  function widgetButtonResize(){
-    $('.widget_sized_btn').each(function(){
-      $(this).height($(this).siblings('.sn_info').outerHeight() + $(this).siblings('.sn_dom').outerHeight())
-    })
-  }
-  widgetButtonResize();
+  // // set the clickable button of shortname
+  // function widgetButtonResize(){
+  //   $('.widget_sized_btn').each(function(){
+  //     $(this).height($(this).siblings('.sn_info').outerHeight() + $(this).siblings('.sn_dom').outerHeight())
+  //   })
+  // }
+  // widgetButtonResize();
 
-  $( window ).on( "resize", function() {
-    widgetButtonResize();
-  });
+  // $( window ).on( "resize", function() {
+  //   widgetButtonResize();
+  // });
   
 });
--- js/masonry.js
+++ js/masonry.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
     horizontalOrder: true,
     percentPosition: true
   });
+
+  // set the clickable button of shortname
+  function widgetButtonResize(){
+    $('.widget_sized_btn').each(function(){
+      $(this).height($(this).siblings('.sn_info').outerHeight() + $(this).siblings('.sn_dom').outerHeight())
+    })
+  }
+  widgetButtonResize();
+
+  $( window ).on( "resize", function() {
+    widgetButtonResize();
+  });
 });
